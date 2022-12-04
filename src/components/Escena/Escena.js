f0c84ab4311454a885db392126f9d2769c640a37
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-12-04 14:14:57 +0100

    feat: exercise 1

---
+++ src/components/Escena/Escena.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Escena = () => {
+  const frase = 'El nostre heroi estava surant per l\'espai sideral quan a la llunyania va albirar una nau espacial'
+  return (
+    <p>
+      {frase}
+    </p>
+  )
+}
+
+export default Escena
