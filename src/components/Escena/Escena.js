956dee463cfe293308e7205d8d8092cc52511633
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-12-04 16:07:31 +0100

    feat: added selected sentence function (ex4)

--- src/components/Escena/Escena.js
+++ src/components/Escena/Escena.js
@@ -1,12 +1,38 @@
 import React from 'react'
-import { Button } from './EscenaStyled'
+import { ButtonGroup, Sentence } from './EscenaStyled'
 
 const Escena = ({ sentences }) => {
+  const [isSelected, setIsSelected] = React.useState(0);
+
+  function previousSentence() {
+    setIsSelected(prevState =>
+      prevState = (isSelected > 0) ? --prevState : sentences.length - 1
+    );
+  }
+
+  function nextSentence() {
+    setIsSelected(prevState =>
+      prevState = (isSelected < sentences.length-1) ? ++prevState : 0
+    );
+  }
+
   return (
     <div>
-      { sentences.map(sentence =>
-        <Button>{sentence}</Button>
-      ) }
+      <ButtonGroup>
+        <button onClick={previousSentence}>Anterior</button>
+        <button onClick={nextSentence}>Següent</button>
+      </ButtonGroup>
+      {sentences.map(sentence => {
+        let index = sentences.indexOf(sentence);
+        return (
+          <Sentence
+            key={sentence}
+            primary={index === isSelected}
+          >
+            {sentence}
+          </Sentence>
+        )
+      })}
     </div>
   )
 }
--- src/components/Escena/EscenaStyled.js
+++ src/components/Escena/EscenaStyled.js
@@ -1,8 +1,17 @@
 import styled from 'styled-components'
 
-export const Button = styled.p`
+export const ButtonGroup = styled.div`
+   display: grid;
+   grid-template-columns: repeat(2, 1fr);
+   gap: 1rem;
+   padding: 1rem;
+   width: 100%;
+`;
+
+export const Sentence = styled.p`
+   background: ${props => props.primary ? "darkOliveGreen" : "transparent"};
    border: 1px solid gray;
    border-radius: 2rem;
-   color: rgba(255,255,255,0.87);
+   color: rgba(255, 255, 255, 0.87);
    padding: 1rem;
-`
+ `;
