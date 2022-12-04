170125cba562ba1a73f317d4303d8da03314bef2
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-12-04 15:13:08 +0100

    exercise 2 - data json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import data from './data.js';
 import Escena from './components/Escena/Escena'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Escena />
+    <div className='App'>
+      <header className='App-header'>
+        <Escena sentences={data} />
       </header>
     </div>
   )
--- src/components/Escena/Escena.js
+++ src/components/Escena/Escena.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-const Escena = () => {
-  const frase = 'El nostre heroi estava surant per l\'espai sideral quan a la llunyania va albirar una nau espacial'
+const Escena = ({sentences}) => {
   return (
-    <p>
-      {frase}
-    </p>
+    <div>
+      {sentences.map(sentence => <p>{sentence}</p>)}
+    </div>
   )
 }
 
