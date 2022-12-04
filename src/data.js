6f992d9fdf38904da9b08f7e2e17fabc9915b799
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-12-04 16:12:33 +0100

    feat: added images

--- src/data.js
+++ src/data.js
@@ -1,8 +1,20 @@
 const data = [
-  "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
-  "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-  "L'heroi va decidir travessar la porta que el portava a casa",
-  "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ..."
+  {
+    txt: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    img: '../public/images/1.jpg'
+  },
+  {
+    txt: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    img: '../public/images/2.jpg'
+  },
+  {
+    txt: "L'heroi va decidir travessar la porta que el portava a casa",
+    img: '../public/images/3.jpg'
+  },
+  {
+    txt: "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ...",
+    img: '../public/images/4.jpg'
+  }
 ]
 
 export default data
