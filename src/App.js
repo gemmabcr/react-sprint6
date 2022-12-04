1ffb1a1b3fefd4f51ca34ee5b0c7d4d6b20d2e16
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-12-04 16:10:04 +0100

    feat: added intro elements (ex5)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import './App.css'
-import data from './data.js';
+import data from './data.js'
 import Escena from './components/Escena/Escena'
 
 function App() {
+  const [visibleEscena, setVisibleEscena] = React.useState(false)
+
+  const title = 'S6. Intro React'
+  const subtitle = 'Això és un tutorial en el qual mitjançant dos botons podràs avançar i retrocedir en els consells.'
+  const buttonText = 'Començar'
+
+  function showEscena() { setVisibleEscena(prevState => !prevState) }
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <Escena sentences={data} />
+        <h2>{title}</h2>
+        <p>{subtitle}</p>
+        {!visibleEscena && <button onClick={showEscena}>{buttonText}</button>}
+        {visibleEscena && <Escena sentences={data} />}
       </header>
     </div>
   )
